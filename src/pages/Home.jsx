b786bcc3e7f73b2375f8f5f7bb1921ede4a98da7
manishcodesq <manishkulkarni375@gmail.com>
2025-07-29 22:47:53 +0530

    imported react dom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Box, Typography, Button, Grid, Paper, Stack } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import PsychologyIcon from '@mui/icons-material/Psychology';
